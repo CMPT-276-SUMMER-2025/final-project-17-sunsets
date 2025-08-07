88a82f032fc222a700053a097e73e905bfd980bb
Jacob Yan <jty10@sfu.ca>
2025-08-06 17:08:47 -0700

    imported testing libraries

---
+++ frontend/test/MapView.test.jsx
@@ -0,0 +1,5 @@
+import { render, screen, waitFor, fireEvent } from '@testing-library/react'
+import { vi } from 'vitest'
+import MapView from '..src/Components/MapView/MapView'
+import '@testing-library/jest-dom/vitest'
+
